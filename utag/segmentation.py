83d94241634f31b6cb7762d350b49303a9438e94
jkim810 <jkim0810@gmail.com>
2021-12-30 16:05:05 -0500

    refractoring core functions

--- utag/segmentation.py
+++ utag/segmentation.py
@@ -14,21 +14,17 @@ from scipy.sparse import (spdiags, SparseEfficiencyWarning, csc_matrix,
 import anndata
 warnings.simplefilter('ignore',SparseEfficiencyWarning)
 
-
-def segment_slide(
+def UTAG(
     adata: AnnData,
     channels_to_use: Array = None,
     slide_key: str = 'Slide', # None in case only one slide
-    batch_key: str = 'sample',
     save_key: str = 'UTAG Label',
-    filter_by_variance:bool = False,
-    normalize_adjacency_matrix: bool = False,
-    max_dist: float = 1e20,
-    kernel: str = None,
-    sigma: float = 1.0,
-    gamma: float = 10.0,
-    n_pcs: int = 10,
+    filter_by_variance: bool = False,
+    max_dist: float = 20, # Recommended value of 20 ~ 50 depending on magnification
+    normalization_mode: str = 'l1_norm',
+    parc_pcs: int = 10,
     apply_umap: bool = False,
+    umap_gamma: float = 10.0,
     apply_clustering: bool = True,
     clustering_method: Array = ['leiden', 'parc'],
     resolutions: Array = [0.05, 0.1, 0.3, 1.0]
@@ -67,24 +63,22 @@ def segment_slide(
 
             sq.gr.spatial_neighbors(ad_copy, radius = max_dist, coord_type = 'generic', set_diag = True)
             
-            ad_copy = segment_graph(ad_copy, sigma = sigma, normalize_adjacency_matrix = normalize_adjacency_matrix, kernel = kernel)
+            ad_copy = custom_message_passing(ad_copy, mode = normalization_mode)
             ad_list.append(ad_copy)
         ad_result = anndata.concat(ad_list)
     else:
         sq.gr.spatial_neighbors(ad, radius = max_dist, coord_type = 'generic', set_diag = True)
-        ad_result = segment_graph(ad, sigma = sigma, kernel = kernel)
+        ad_result = custom_message_passing(ad, mode = normalization_mode)
     
     if apply_clustering:
-        import bbknn, parc
-        sc.tl.pca(ad_result, n_comps = n_pcs)
-        sc.pp.neighbors(ad_result, n_pcs = n_pcs)
-        #bbknn.bbknn(ad_result, batch_key = batch_key, n_pcs = n_pcs)
+        sc.tl.pca(ad_result, n_comps = parc_pcs)
+        sc.pp.neighbors(ad_result, n_pcs = parc_pcs)
+
         if apply_umap:
             print('Running UMAP on Input Dataset...')
-            sc.tl.umap(ad_result, gamma = gamma)
+            sc.tl.umap(ad_result, gamma = umap_gamma)
         
         for resolution in tqdm(resolutions):
-            
             
             res_key1 = save_key + '_leiden_' + str(resolution)
             res_key2 = save_key + '_parc_' + str(resolution)
@@ -101,59 +95,18 @@ def segment_slide(
             
     return ad_result 
 
-def segment_graph(
+def custom_message_passing(
     adata: AnnData,
-    kernel: str = 'rbf',
-    sigma: float = 1.0,
-    normalize_adjacency_matrix: bool = False
+    mode: str = 'l1_norm'
 ) -> AnnData:
     
     from scipy.linalg import sqrtm
-    
-    # Radial Basis Function
-    if kernel == 'rbf':
-        #fully_connected = adata.copy()
-        # radius supposed to be very large number
-        #sq.gr.spatial_neighbors(fully_connected, radius = 1e100, coord_type = 'generic')
-
-        A = adata.obsp['spatial_distances']
-        A_mod = A
-        affinity = np.exp(-(A_mod/(2 * sigma**2)))
-        
-        #A_mod = A + np.eye(A.shape[0])
-        #affinity = affinity * A_mod
-        connectivity = adata.obsp['spatial_connectivities'] + np.eye(adata.obsp['spatial_connectivities'].shape[0])
-        affinity = affinity * connectivity
-    elif kernel == 'inverse_quadratic':
-        #fully_connected = adata.copy()
-        # radius supposed to be very large number
-        #sq.gr.spatial_neighbors(fully_connected, radius = 1e100, coord_type = 'generic')
-
-        A = adata.obsp['spatial_distances']
-        A_mod = A
-        affinity = 1/(1+sigma ** 2 * A_mod)
-        
-        connectivity = adata.obsp['spatial_connectivities'] + np.eye(adata.obsp['spatial_connectivities'].shape[0])
-        affinity = affinity * connectivity
-    elif normalize_adjacency_matrix:
-        # D for A_mod:
+    if mode == 'l1_norm':
         A = adata.obsp['spatial_connectivities']
         A_mod = A + np.eye(A.shape[0])
         
-        '''
-        D_mod = np.zeros_like(A_mod)
-        np.fill_diagonal(D_mod, A_mod.sum(axis=1).flatten())
-
-        # Inverse square root of D:
-        D_mod_invroot = np.linalg.inv(sqrtm(D_mod))
-        
-        # technically not affinity
-        affinity = D_mod_invroot @ A_mod @ D_mod_invroot
-        '''
-        #affinity = A_mod / A_mod.sum(axis=1)[:,np.newaxis]
         from sklearn.preprocessing import normalize
         affinity = normalize(A_mod, axis=1, norm='l1')
-        
     else:
         # Plain A_mod multiplication
         A = adata.obsp['spatial_connectivities']
@@ -162,46 +115,10 @@ def segment_graph(
     adata.X = affinity @ adata.X
     return adata
 
-
-def segment_graph_backup(
-    adata: AnnData,
-    sigma: float = 10.0,
-    n_pcs: int = 10,
-    resolution: float = 0.5,
-    kernel: str = 'rbf',
-    apply_clustering: bool = True
-) -> AnnData:
-    
-    #A = adata.obsp['spatial_distances']
-    A = adata.obsp['spatial_distances']
-    A_mod = A + np.eye(A.shape[0])
-    
-    # Radial Basis Function
-    if kernel == 'rbf':
-        affinity = np.exp(-((sigma)**2 * A_mod))
-    if kernel == 'inverse_quadratic':
-        affinity = 1/(1+sigma ** 2 * A_mod)
-    
-    adata.X = affinity @ adata.X
-    
-    if apply_clustering:
-        sc.pp.neighbors(adata, n_pcs = n_pcs)
-        sc.tl.umap(adata)
-        sc.tl.leiden(adata, resolution = resolution)
-        freq = adata.obs.groupby('leiden').count()
-        
-        freq = freq / freq.sum()
-        low_freq = freq.loc[freq['area'] < 0.01,:].index
-        low_freq = {key: 'Empty' for key in low_freq}
-        adata.obs['leiden'] = adata.obs['leiden'].replace(low_freq)
-
-    return adata
-
 def low_variance_filter(
     adata: AnnData
 ) -> AnnData:
     return adata[:,adata.var['std']  > adata.var['std'].median()]
-
 
 def evaluate_performance(
     adata: AnnData,
